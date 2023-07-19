94bb5e0a827e69d3621f16daf45ddaded65b69b8
Prerona <preronaghosh1997@gmail.com>
2023-07-19 18:06:27 -0400

    adding ovs rules for part-b

---
+++ part-B/partB_connect.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+# Sets bridge s1 to use OpenFlow 1.3
+ovs-vsctl set bridge s1 protocols=OpenFlow13 
+
+# Sets bridge s2 to use OpenFlow 1.3
+ovs-vsctl set bridge s2 protocols=OpenFlow13
+
+# Sets bridge s3 to use OpenFlow 1.3
+ovs-vsctl set bridge s3 protocols=OpenFlow13
+
+# Sets bridge r1 to use OpenFlow 1.3
+ovs-vsctl set bridge r1 protocols=OpenFlow13
+
+# Sets bridge r2 to use OpenFlow 1.3
+ovs-vsctl set bridge r2 protocols=OpenFlow13
+
+# Print the protocols that each switch supports
+for switch in s1 s2 s3 r1 r2;
+do
+    protos=$(ovs-vsctl get bridge $switch protocols)
+    echo "Switch $switch supports $protos"
+done
+
+# Avoid having to write "-O OpenFlow13" before all of your ovs-ofctl commands.
+ofctl='ovs-ofctl -O OpenFlow13'
+
+
+# ----------------------- Alice and Bob -------------------- #
+
+# OVS rules for Switch 1
+$ofctl add-flow s1 \
+    nw_src=10.1.1.17,nw_dst=10.4.4.48,dl_src=0A:00:00:02:00:00,actions=mod_dl_src:0A:00:0A:01:00:02,mod_dl_dst:0A:00:04:01:00:01,output=2
+
+$ofctl add-flow s1 \
+    nw_src=10.4.4.48,nw_dst=10.1.1.17,dl_src=0A:00:04:01:00:01,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:0A:00:00:02:00:00,output=1
+
+
+# OVS rules for Switch 2
+$ofctl add-flow s2 \
+    nw_src=10.1.1.17,nw_dst=10.4.4.48,dl_src=0A:00:0E:FE:00:02,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
+
+$ofctl add-flow s2 \
+    nw_src=10.4.4.48,nw_dst=10.1.1.17,dl_src=0A:00:01:02:00:00,actions=mod_dl_src:0A:00:0A:FE:00:02,mod_dl_dst:0A:00:0E:FE:00:02,output=2
+
+
+# OVS rules for R1
+$ofctl add-flow r1 \
+    dl_src=0A:00:0A:01:00:02,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:0A:FE:00:02,output=2
+
+$ofctl add-flow r1 \
+    dl_src=0A:00:0A:FE:00:02,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:0A:01:00:02,output=1
+
+
+# ----------------------- Carol and Bob -------------------- #
+
+# OVS rules for Switch 2
+$ofctl add-flow s2 \
+    nw_src=10.4.4.48,nw_dst=10.6.6.69,dl_src=0A:00:01:02:00:00,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:05:01:00:01,output=3
+
+$ofctl add-flow s2 \
+    nw_src=10.6.6.69,nw_dst=10.4.4.48,dl_src=0A:00:05:01:00:01,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
+
+
+# OVS rules for Switch 3
+$ofctl add-flow s3 \
+    nw_src=10.4.4.48,nw_dst=10.6.6.69,dl_src=0A:00:10:FE:00:02,actions=mod_dl_src:0A:00:02:01:00:01,mod_dl_dst:0A:00:02:02:00:00,output=1
+
+$ofctl add-flow s3 \
+    nw_src=10.6.6.69,nw_dst=10.4.4.48,dl_src=0A:00:02:02:00:00,actions=mod_dl_src:0A:00:0B:FE:00:02,mod_dl_dst:0A:00:10:FE:00:02,output=2
+
+
+# OVS rules for R2
+$ofctl add-flow r2 \
+    dl_src=0A:00:0C:01:00:03,actions=mod_dl_src:0A:00:10:FE:00:02,mod_dl_dst:0A:00:0B:FE:00:02,output=2
+
+$ofctl add-flow r2 \
+    dl_src=0A:00:0B:FE:00:02,actions=mod_dl_src:0A:00:05:01:00:01,mod_dl_dst:0A:00:0C:01:00:03,output=1
+
+
+# Print the flows installed in each switch
+for switch in s1 s2 s3 r1 r2;
+do
+    echo "Flows installed in $switch:"
+    $ofctl dump-flows $switch
+    echo ""
+done
