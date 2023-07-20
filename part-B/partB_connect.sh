567a2d0a16bc8d129df7b8684c1f874dacae5aa5
Prerona <preronaghosh1997@gmail.com>
2023-07-20 14:09:43 -0400

    Fixed ovs rule for partB

--- part-B/partB_connect.sh
+++ part-B/partB_connect.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 # Sets bridge s1 to use OpenFlow 1.3
 ovs-vsctl set bridge s1 protocols=OpenFlow13 
@@ -26,58 +26,67 @@ done
 ofctl='ovs-ofctl -O OpenFlow13'
 
 
-# ----------------------- Alice and Bob -------------------- #
+# ----------------------- Alice to Bob -------------------- #
 
-# OVS rules for Switch 1
+# OVS rules for S1
 $ofctl add-flow s1 \
-    nw_src=10.1.1.17,nw_dst=10.4.4.48,dl_src=0A:00:00:02:00:00,actions=mod_dl_src:0A:00:0A:01:00:02,mod_dl_dst:0A:00:04:01:00:01,output=2
-
-$ofctl add-flow s1 \
-    nw_src=10.4.4.48,nw_dst=10.1.1.17,dl_src=0A:00:04:01:00:01,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:0A:00:00:02:00:00,output=1
-
-
-# OVS rules for Switch 2
-$ofctl add-flow s2 \
-    nw_src=10.1.1.17,nw_dst=10.4.4.48,dl_src=0A:00:0E:FE:00:02,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
-
-$ofctl add-flow s2 \
-    nw_src=10.4.4.48,nw_dst=10.1.1.17,dl_src=0A:00:01:02:00:00,actions=mod_dl_src:0A:00:0A:FE:00:02,mod_dl_dst:0A:00:0E:FE:00:02,output=2
-
+    ip,nw_dst=10.4.4.48,actions=mod_dl_src:0A:00:0A:01:00:02,mod_dl_dst:0A:00:04:01:00:01,output:2
 
 # OVS rules for R1
 $ofctl add-flow r1 \
-    dl_src=0A:00:0A:01:00:02,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:0A:FE:00:02,output=2
+    dl_src=0A:00:0A:01:00:02,actions=mod_dl_src:0A:00:0E:FE:00:02,mod_dl_dst:0A:00:0A:FE:00:02,output:2
 
+# OVS rules for S2
+$ofctl add-flow s2 \
+    ip,nw_src=10.1.1.17,nw_dst=10.4.4.48,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output:1
+
+
+# ----------------------- Bob to Alice -------------------- #
+
+# OVS rules for S2
+$ofctl add-flow s2 \
+    ip,nw_src=10.4.4.48,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:0A:FE:00:02,mod_dl_dst:0A:00:10:FE:00:02,output:2
+
+# OVS rules for R1
 $ofctl add-flow r1 \
-    dl_src=0A:00:0A:FE:00:02,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:0A:01:00:02,output=1
+    dl_src=0A:00:0A:FE:00:02,actions=mod_dl_src:0A:00:04:01:00:01,mod_dl_dst:0A:00:0A:01:00:02,output:1
+
+# OVS rules for S1
+$ofctl add-flow s1 \
+    ip,nw_dst=10.1.1.17,actions=mod_dl_src:0A:00:00:01:00:01,mod_dl_dst:0A:00:00:02:00:00,output:1
 
 
-# ----------------------- Carol and Bob -------------------- #
+# ----------------------- Bob to Carol -------------------- #
 
-# OVS rules for Switch 2
+# OVS rules for S2
 $ofctl add-flow s2 \
-    nw_src=10.4.4.48,nw_dst=10.6.6.69,dl_src=0A:00:01:02:00:00,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:05:01:00:01,output=3
-
-$ofctl add-flow s2 \
-    nw_src=10.6.6.69,nw_dst=10.4.4.48,dl_src=0A:00:05:01:00:01,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output=1
-
-
-# OVS rules for Switch 3
-$ofctl add-flow s3 \
-    nw_src=10.4.4.48,nw_dst=10.6.6.69,dl_src=0A:00:10:FE:00:02,actions=mod_dl_src:0A:00:02:01:00:01,mod_dl_dst:0A:00:02:02:00:00,output=1
-
-$ofctl add-flow s3 \
-    nw_src=10.6.6.69,nw_dst=10.4.4.48,dl_src=0A:00:02:02:00:00,actions=mod_dl_src:0A:00:0B:FE:00:02,mod_dl_dst:0A:00:10:FE:00:02,output=2
-
+    ip,nw_dst=10.6.6.69,dl_src=0A:00:01:02:00:00,actions=mod_dl_src:0A:00:0C:01:00:03,mod_dl_dst:0A:00:05:01:00:01,output:3
 
 # OVS rules for R2
 $ofctl add-flow r2 \
-    dl_src=0A:00:0C:01:00:03,actions=mod_dl_src:0A:00:10:FE:00:02,mod_dl_dst:0A:00:0B:FE:00:02,output=2
+    dl_src=0A:00:0C:01:00:03,actions=mod_dl_src:0A:00:10:FE:00:02,mod_dl_dst:0A:00:0B:FE:00:02,output:2
 
+# OVS rules for S3
+$ofctl add-flow s3 \
+    ip,nw_dst=10.6.6.69,dl_src=0A:00:10:FE:00:02,actions=mod_dl_src:0A:00:02:01:00:01,mod_dl_dst:0A:00:02:02:00:00,output:1
+
+
+# ----------------------- Carol to Bob -------------------- #
+
+# OVS rules for S3
+$ofctl add-flow s3 \
+    ip,nw_dst=10.4.4.48,dl_src=0A:00:02:02:00:00,actions=mod_dl_src:0A:00:0B:FE:00:02,mod_dl_dst:0A:00:10:FE:00:02,output:2
+
+# OVS rules for R2
 $ofctl add-flow r2 \
-    dl_src=0A:00:0B:FE:00:02,actions=mod_dl_src:0A:00:05:01:00:01,mod_dl_dst:0A:00:0C:01:00:03,output=1
+    dl_src=0A:00:0B:FE:00:02,actions=mod_dl_src:0A:00:05:01:00:01,mod_dl_dst:0A:00:0C:01:00:03,output:1
+
+# OVS rules for S2
+$ofctl add-flow s2 \
+    ip,nw_src=10.6.6.69,dl_src=0A:00:05:01:00:01,actions=mod_dl_src:0A:00:01:01:00:01,mod_dl_dst:0A:00:01:02:00:00,output:1
 
 
+	
 # Print the flows installed in each switch
 for switch in s1 s2 s3 r1 r2;
 do
